a76a021f23573ec7bee8167f65d1f9fda90d5f2f
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-11-04 15:09:59 +0500

    Componenet and Elements

--- 5-lessonComponentsElements/my-app/src/App.js
+++ 5-lessonComponentsElements/my-app/src/App.js
@@ -1,9 +1,37 @@
 import './App.css';
 
+const Header=()=>{
+  const sayHello =(uname)=>{
+    return(
+      <div>
+        <h1>Welcome to {uname}</h1>
+        </div>
+    )
+  }
+  return (
+      <div>
+        {sayHello('Abdulbosit')}
+      </div>
+  )
+}
+
+const Field=()=>{
+  return(
+    <input type='text' placeholder='new componenet'/>
+  )
+}
+
+const Button = ()=>{
+ const text ='Click'
+ return <button>{text} </button>
+}
+
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <Header/>
+      <Field/> <Button/>
     </div>
     
   );
