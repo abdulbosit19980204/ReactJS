950032dff4dadb4eee92aa90d8c37c7d6e5876ea
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-11-13 00:40:57 +0500

    Component Xolati States

---
+++ 13-dars-ComponentXolatiVaClass/src/components/deletable/deletable.js
@@ -0,0 +1,16 @@
+// Class components
+
+import React from "react";
+import deletable from "./deletable"
+
+class User extends React.Component{
+  render(){
+   this.render(
+    <div>
+    <h1>Limit</h1>
+</div>
+   )
+  }  
+}
+
+export default eletable
