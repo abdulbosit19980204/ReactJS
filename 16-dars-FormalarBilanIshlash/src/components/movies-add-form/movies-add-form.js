f64e129d44a97de69c5661cf652b95afe4bf02fd
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-11-15 16:57:01 +0500

    Event target movies add form input values

--- 16-dars-FormalarBilanIshlash/src/components/movies-add-form/movies-add-form.js
+++ 16-dars-FormalarBilanIshlash/src/components/movies-add-form/movies-add-form.js
@@ -1,19 +1,38 @@
-
+import { Component } from 'react'
 import './movies-add-form.css'
 
-const MoviesAddForm = ()=>{
-    return(
-        <div className='movies-add-form'>
-            <h3>Yangi kino qoshish</h3>
-            <form className='add-form d-flex'>
-            <input type="text" className="form-control new-post-lable" placeholder="Qanday kino?"/>
-            <input type="number" className="form-control new-post-lable" placeholder="Nechi marotaba korilgan?"/>
-            <button type='submit' className='btn btn-outline-dark'>
-                Qo'shish
-            </button>
-            </form>
-        </div>
-    )
+
+class MoviesAddForm extends Component {
+    constructor(props){
+        super(props)
+       this.state= {
+        filmName:'',
+        viewers:''
+        }
+    }
+
+
+changeInputHandler = (e)=>{
+this.setState({
+    [e.target.name]:e.target.value,
+})
+}
+
+
+    render(){
+        return(
+            <div className='movies-add-form'>
+                <h3>Yangi kino qoshish</h3>
+                <form className='add-form d-flex'>
+                <input onChange={this.changeInputHandler} name='filmName' type="text" className="form-control new-post-lable" placeholder="Qanday kino?"/>
+                <input onChange={this.changeInputHandler} name='viewers' type="number" className="form-control new-post-lable" placeholder="Nechi marotaba korilgan?"/>
+                <button type='submit' className='btn btn-outline-dark'>
+                    Qo'shish
+                </button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default MoviesAddForm
