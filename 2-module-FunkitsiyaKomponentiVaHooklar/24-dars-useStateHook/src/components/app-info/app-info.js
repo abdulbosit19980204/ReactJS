28f9e6e5eabbeefaaf5a4e6bb6e5bf952e5d9929
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-11-25 00:41:30 +0500

    useStateHook

---
+++ 2-module-FunkitsiyaKomponentiVaHooklar/24-dars-useStateHook/src/components/app-info/app-info.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './app-info.scss'
+
+const AppInfo = ({allMoviesCount, favouriteMovieCount}) => {
+  
+  return (
+    <div className='app-info'>
+      <p style={{fontSize: 50, fontWeight:800}}>Barcha kinolar soni: {allMoviesCount}</p>
+      <p className='fs-3 text-uppercase'>Sevimli kinolar soni: {favouriteMovieCount}</p>
+
+    </div>
+  )
+}
+
+export default AppInfo
