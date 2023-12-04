17bb0771195c0c96894586edd5210cbf6c52c076
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-12-04 12:39:00 +0500

    Life cycle method

---
+++ 2-module-FunkitsiyaKomponentiVaHooklar/25-dars-LifeCycleMethod/src/components/movie-list/movie-list.js
@@ -0,0 +1,16 @@
+import './movie-list.css'
+import MovieListItem from '../movie-list-item/movie-list-item'
+const MovieList = ({data})=>{
+    return <ul className="movie-list">
+        {data.map(item=>(
+        <MovieListItem key ={item.id} filmName={item.filmName} viewers={item.viewers} favourite={item.favourite}></MovieListItem>
+
+        // <MovieListItem key={item.id} { ...item}/>
+        ))}
+        
+
+    </ul>
+
+}
+
+export default MovieList
