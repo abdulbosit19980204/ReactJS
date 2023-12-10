22ba2888f243d4f5d8c51748fd81bf376cb2752e
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-12-10 22:23:22 +0500

    Search

--- test/src/sections/customer/searched.js
+++ test/src/sections/customer/searched.js
@@ -11,83 +11,108 @@ import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 import Checkbox from '@mui/material/Checkbox';
+import { useState, useCallback } from 'react';
 
 export const SearchedItems = () => {
 
  
+  const [isShowed, setIsDisplay]=useState(false)
   
   const onAddHandle = ()=>{
    
     console.log("Qo'shish");
+setIsDisplay(!isShowed)
    
   }
+
+  const  disableShow = () =>{
+    setIsDisplay(!isShowed)
+  }
+
+
   return (
    <Stack sx={{ width: '100%' }} spacing={2}>
         <Alert severity="primary">
           <AlertTitle><YouTube fontSize="small" color='error' /> Youtube</AlertTitle>
           This is an error alert — <strong>check it out!</strong>
-          <ul>
-            <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
-            <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
-            <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
-            <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
-            <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
-          </ul>
+          {
+            !isShowed && (
+              <ul>
+                <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
+                <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
+                <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
+                <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
+                <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
+              </ul>
+            )
+          }
           <br />
           <ButtonGroup variant="text" aria-label="text button group" size='small'>
             <Button onClick={onAddHandle}><AddIcon /></Button>
-            <Button><RemoveIcon/></Button>
+            <Button onClick={disableShow}><RemoveIcon/></Button>
             <Button><RemoveRedEyeIcon/></Button>
           </ButtonGroup>
         </Alert>
         <Alert severity="primary">
           <AlertTitle><Instagram fontSize="small" color='secondary' /> Instagram</AlertTitle>
           This is a warning alert — <strong>check it out!</strong>
-          <ul>
-            <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
-            <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
-            <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
-            <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
-            <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
-          </ul>
+          {
+            isShowed && (
+              <ul>
+                <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
+                <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
+                <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
+                <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
+                <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
+              </ul>
+            )
+          }
           <br />
           <ButtonGroup variant="text" aria-label="text button group" size='small'>
             <Button onClick={onAddHandle}><AddIcon /></Button>
-            <Button><RemoveIcon/></Button>
+            <Button onClick={disableShow}><RemoveIcon/></Button>
             <Button><RemoveRedEyeIcon/></Button>
           </ButtonGroup>
         </Alert>
         <Alert severity="primary">
           <AlertTitle><Telegram fontSize="small" color='info' /> Telegram</AlertTitle>
           This is an info alert — <strong>check it out!</strong>
-         <ul>
-          <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
-          <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
-          <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
-          <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
-          <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
-         </ul>
+          {
+            isShowed && (
+              <ul>
+                <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
+                <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
+                <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
+                <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
+                <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
+              </ul>
+            )
+          }
          <br />
           <ButtonGroup variant="text" aria-label="text button group" size='small'>
             <Button onClick={onAddHandle}><AddIcon /></Button>
-            <Button><RemoveIcon/></Button>
+            <Button onClick={disableShow}><RemoveIcon/></Button>
             <Button><RemoveRedEyeIcon/></Button>
           </ButtonGroup>
         </Alert>
         <Alert severity="primary">
           <AlertTitle><WhatsApp fontSize="small" color='success' /> WhatsApp</AlertTitle>
           This is a success alert — <strong>check it out!</strong>
-         <ul>
-          <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
-          <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
-          <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
-          <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
-          <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
-         </ul>
+          {
+            isShowed && (
+              <ul>
+                <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
+                <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
+                <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
+                <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
+                <li> <Checkbox size='small' /><a href="#">Tuychiev Abdulbosit</a></li>
+              </ul>
+            )
+          }
          <br />
           <ButtonGroup variant="text" aria-label="text button group"  size='small'>
             <Button onClick={onAddHandle}><AddIcon /></Button>
-            <Button><RemoveIcon/></Button>
+            <Button onClick={disableShow}><RemoveIcon/></Button>
             <Button><RemoveRedEyeIcon/></Button>
           </ButtonGroup>
         </Alert>
